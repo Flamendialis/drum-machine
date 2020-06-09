177922f53ba9211da9a047f0b9fe397ca27c5eca
Flamen_Dialis <menendezrfelixj@gmail.com>
2020-06-09 13:23:15 -0300

    Updates

---
+++ precache-manifest.4595027fb723a455ceb00a78ab4c03af.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "750622dfd289ad0b698625ff6978efe8",
+    "url": "/drum-machine/index.html"
+  },
+  {
+    "revision": "8788a22cb1de776d466f",
+    "url": "/drum-machine/static/css/main.52cad3a7.chunk.css"
+  },
+  {
+    "revision": "83c5452dc2ed526ee36d",
+    "url": "/drum-machine/static/js/2.96cbdf93.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/drum-machine/static/js/2.96cbdf93.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8788a22cb1de776d466f",
+    "url": "/drum-machine/static/js/main.c9c7981d.chunk.js"
+  },
+  {
+    "revision": "fc1b3e84514a8c25bf54",
+    "url": "/drum-machine/static/js/runtime-main.1e07d996.js"
+  },
+  {
+    "revision": "85d40ea8d25d79a7b0d003e5c10deaed",
+    "url": "/drum-machine/static/media/CFLCD-Regular.85d40ea8.ttf"
+  }
+]);
